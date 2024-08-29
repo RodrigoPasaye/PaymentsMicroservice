d04d6d340cb6bb4621f6c5aa0ad84c4e9eee55ec
RodrigoPasaye <rodrigo.pasaye@cmv.mx>
2024-08-28 21:16:17 -0600

    Se retorna solo la info necesaria en createPaymentSession
    ** Se termina el stripeWebhook

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { envs } from 'src/config';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { envs, NATS_SERVICE } from 'src/config';
 import Stripe from 'stripe';
 import { PaymentSessionDto } from './dto/payment-session.dto';
 import { Request, Response } from 'express';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
 
   private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger('PaymnetsMicroservice');
+
+  constructor(
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
+  ) {}
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
 
@@ -39,7 +45,13 @@ export class PaymentsService {
       cancel_url: envs.stripeCancelUrl,
     });
 
-    return session;
+    // return session;
+
+    return {
+      cancelUrl: session.cancel_url,
+      successUrl: session.success_url,
+      url: session.url,
+    }
   }
 
   async stripeWebhook(req: Request, res: Response) {
@@ -63,12 +75,16 @@ export class PaymentsService {
 
     switch (event.type) {
       case 'charge.succeeded':
-        //TODO: llamar nuestro microservice
         const chargeSucceeded = event.data.object;
-        console.log({
-          metadata: chargeSucceeded.metadata,
-          orderId: chargeSucceeded.metadata.orderId
-        });
+        const payload = {
+          stripePaymentId: chargeSucceeded.id,
+          orderId: chargeSucceeded.metadata.orderId,
+          receiptUrl: chargeSucceeded.receipt_url,
+        }
+
+        // this.logger.log({payload});
+
+        this.client.emit('payment.succeeded', payload);
         break;
 
       default:
